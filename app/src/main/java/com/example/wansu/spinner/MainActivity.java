d6fd85081f4c7e9badecd752d5555ced83e40d31
Wan20 <wandy20number@gmail.com>
2016-04-09 23:23:10 +0700

    First Step

---
+++ app/src/main/java/com/example/wansu/spinner/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.wansu.spinner;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+
+public class MainActivity extends AppCompatActivity {
+    // uicontrols
+    Spinner spCountries;
+    Spinner spBusinessType;
+    Button btnsubmit;
+
+    //class members
+    String businessType[] = { "Automobile", "Food", "Computers", "Education",
+            "Personal", "Travel" };
+    ArrayAdapter<String> adapterBusinessType;
+
+    // local members
+    String sbusinesstype,scountry;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        spCountries = (Spinner) findViewById(R.id.spCountries);
+        spBusinessType = (Spinner) findViewById(R.id.spBussinessType);
+
+        btnsubmit=(Button)findViewById(R.id.submit);
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
