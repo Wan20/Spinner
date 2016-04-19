e0bc6131b468fe06d63833456dd6a509780951ec
Wan20 <wandy20number@gmail.com>
2016-04-19 21:58:59 +0700

    Get Spinner value when Button Click

--- app/src/main/java/com/example/wansu/spinner/MainActivity.java
+++ app/src/main/java/com/example/wansu/spinner/MainActivity.java
@@ -1,16 +1,14 @@
 package com.example.wansu.spinner;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     // uicontrols
@@ -35,31 +33,61 @@ public class MainActivity extends AppCompatActivity {
 
         spCountries = (Spinner) findViewById(R.id.spCountries);
         spBusinessType = (Spinner) findViewById(R.id.spBussinessType);
+        btnsubmit = (Button)findViewById(R.id.button);
 
-        btnsubmit=(Button)findViewById(R.id.submit);
+        // Initialize and set Adapter
+        adapterBusinessType = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_spinner_item, businessType);
+        spBusinessType.setAdapter(adapterBusinessType);
 
-    }
+        /* Country Item Selected Listener
+        spCountries.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
+        {
+            @Override
+            public void onItemSelected(AdapterView<?> adapter, View v, int position, long id)
+            {
+                // On selecting a spinner item
+                String item = adapter.getItemAtPosition(position).toString();
+                // Showing selected spinner item
+                Toast.makeText(getApplicationContext(),
+                        "Selected Country : " + item, Toast.LENGTH_LONG).show();
+            }
 
+            @Override
+            public void onNothingSelected(AdapterView<?> arg0) {
+                // TODO Auto-generated method stub
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+            }
+        });
+        // Business Type Item Selected Listener
+        spBusinessType.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
+        {
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+            @Override
+            public void onItemSelected(AdapterView<?> adapter, View v, int position, long id)
+            {
+                // On selecting a spinner item
+                String item = adapter.getItemAtPosition(position).toString();
+                // Showing selected spinner item
+                Toast.makeText(getApplicationContext(), "Bussiness Type : " + item, Toast.LENGTH_LONG).show();
+            }
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+            @Override
+            public void onNothingSelected(AdapterView<?> arg0)
+            {
+                // TODO Auto-generated method stub
+            }
+        });*/
 
-        return super.onOptionsItemSelected(item);
-    }
+            btnsubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String Text = spCountries.getSelectedItem().toString();
+                Toast.makeText(getApplicationContext(),
+                        "Bussiness Type : " + Text, Toast.LENGTH_LONG).show();
+                String Text2 = spBusinessType.getSelectedItem().toString();
+                Toast.makeText(getApplicationContext(),
+                        "Bussiness Type : " + Text2, Toast.LENGTH_LONG).show();
+            }
+        });
+};
 }
